d24a3b8abdf0785f5366fe8e0b55cbef34fb827f
emol <emol@itu.dk>
2023-11-07 02:03:21 +0100

    more white box

--- dist/content.js
+++ dist/content.js
@@ -2,12 +2,11 @@
 
 
 
+// Create a div element for the white box
 const whiteBox = document.createElement("div");
-const globalStyles = document.createElement("style");
-const textNode = document.createTextNode("Hello, World!");
 
-let isOn = false;
-
+// Set the white box's styles
+whiteBox.style.cursor = "move"; // Set cursor style to indicate draggable
 whiteBox.style.position = "fixed";
 whiteBox.style.bottom = "64px";
 whiteBox.style.right = "64px";
@@ -17,10 +16,50 @@ whiteBox.style.backgroundColor = "white";
 whiteBox.style.border = "1px solid #000";
 whiteBox.style.zIndex = 999
 
-whiteBox.style.color = "black"
 
+// Create a text node with "Hello, World!"
+const textNode = document.createTextNode("Hello, World!");
 
-
-
+// Append the text node to the white box
 whiteBox.appendChild(textNode);
+
+// Append the white box to the body of the page
 document.body.appendChild(whiteBox);
+
+let isDragging = false;
+let offsetX, offsetY;
+
+// Mouse down event to start dragging
+whiteBox.addEventListener("mousedown", function(e) {
+    isDragging = true;
+  
+    // Calculate the offset between the mouse position and the top-left corner of the box
+    offsetX = e.clientX - whiteBox.getBoundingClientRect().left;
+    offsetY = e.clientY - whiteBox.getBoundingClientRect().top;
+  
+    // Set a high z-index to bring the box to the front
+    whiteBox.style.zIndex = "9999"; // You can adjust this value as needed
+  
+    // Prevent default browser behavior for drag-and-drop
+    e.preventDefault();
+  });
+  
+  // Mouse up event to stop dragging
+  document.addEventListener("mouseup", function() {
+    isDragging = false;
+  
+    // Restore the z-index to its original value
+    //whiteBox.style.zIndex = "auto";
+  });
+
+// Mouse move event to update box position while dragging
+document.addEventListener("mousemove", function(e) {
+  if (isDragging) {
+    const newX = e.clientX - offsetX;
+    const newY = e.clientY - offsetY;
+
+    // Update the box's position
+    whiteBox.style.left = newX + "px";
+    whiteBox.style.top = newY + "px";
+  }
+});
