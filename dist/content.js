12319585e1ae3a9c730504507a7a53ef6466e835
emol <emol@itu.dk>
2023-11-07 01:44:56 +0100

    white box

--- dist/content.js
+++ dist/content.js
@@ -1,15 +1,26 @@
 /* global chrome */
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.message === "Procrastinating") {
-        console.log("something")
-        const div = document.createElement("div");
-        div.textContent = "This HTML was injected by the content script.";
-        document.body.appendChild(div);
-        //alert("procrastinating")
-    }
-    else {
-        console.log("somethingElse")
-    }
-  });
 
+
+const whiteBox = document.createElement("div");
+const globalStyles = document.createElement("style");
+const textNode = document.createTextNode("Hello, World!");
+
+let isOn = false;
+
+whiteBox.style.position = "fixed";
+whiteBox.style.bottom = "64px";
+whiteBox.style.right = "64px";
+whiteBox.style.width = "300px";
+whiteBox.style.height = "150px";
+whiteBox.style.backgroundColor = "white";
+whiteBox.style.border = "1px solid #000";
+whiteBox.style.zIndex = 999
+
+whiteBox.style.color = "black"
+
+
+
+
+whiteBox.appendChild(textNode);
+document.body.appendChild(whiteBox);
